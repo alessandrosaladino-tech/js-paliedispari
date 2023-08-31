eb68141393ae108cb4a93baf629ca5fc56cf3ee9
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-08-31 17:06:09 +0200

    Add
    es pari e dispari

--- es_paridispari/script.js
+++ es_paridispari/script.js
@@ -4,28 +4,70 @@ Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzion
 Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 */
 
+// Creo un numero random del pc tramite funzione
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) ;
+}
 
-//Creo una variabile per il numero dell'utente
-const userNumberDOM = document.getElementById('numero-utente');
+const pcNumber = getRndInteger(1, 5);
+console.log(pcNumber);
 
-// Aggiungo un event listener ai due bottoni nel DOM
+function EvenOrOdd(userNumber, pcNumber) {
+    const resultToCheck = pcNumber + userNumber;
+    console.log(pcNumber, "+", userNumber );
 
-document.getElementById('pari').addEventListener('click',function(e){
+    if (resultToCheck % 2 === 0) {
 
-    e.preventDefault();
-    console.log(userNumberDOM.value);
+        return true;
+
+    } else {
+
+        return false;
+
+    }
+
+}
+
+
+// collego i due bottoni del DOM e gli assegno la funzione EvenOrOdd
+
+document.getElementById('pari').addEventListener('click', function (e) {
+    const inputUtenteDOM = document.getElementById('numero_utente').value;
+
+    console.log(inputUtenteDOM);
+    if (inputUtenteDOM < 1 || inputUtenteDOM > 5) {
+        console.log("Hai inserito un numero troppo alto!");
+        alert("Hai inserito un numero non compreso tra 1 e 5. Riprova!")
+    }
+    else if (EvenOrOdd(inputUtenteDOM, pcNumber)) {
+
+        console.log('pari');
+        document.getElementById('result').innerHTML = `Il numero del computer è: ${pcNumber}. Il risultato è pari. HAI VINTO!`
+
+    } else {
+        console.log('dispari');
+        document.getElementById('result').innerHTML = `Il numero del computer è: ${pcNumber}. Il risultato è dispari. HAI PERSO!`
+    }
+
 })
 
-document.getElementById('dispari').addEventListener('click',function(e){
+document.getElementById('dispari').addEventListener('click', function (e) {
+    const inputUtenteDOM = document.getElementById('numero_utente').value;
 
-    e.preventDefault();
-    console.log(userNumberDOM.value);
+    console.log(inputUtenteDOM);
+    if (inputUtenteDOM < 1 || inputUtenteDOM > 5) {
+        console.log("Hai inserito un numero troppo alto!");
+        alert("Hai inserito un numero non compreso tra 1 e 5. Riprova!")
+    }
+    else if (EvenOrOdd(inputUtenteDOM, pcNumber)) {
+
+        console.log('pari');
+        document.getElementById('result').innerHTML = `Il numero del computer è: ${pcNumber}. Il risultato è pari. HAI PERSO!`
+
+    } else {
+        console.log('dispari');
+        document.getElementById('result').innerHTML = `Il numero del computer è: ${pcNumber}. Il risultato è dispari. HAI VINTO!`
+    }
 })
-//Controlliamo se l'utente ha scelto un numero fino a 5 con una condizione
 
-//Creiamo una funzione per generare il numero del computer
-
-//Creo una funzione che sommi il numero dell'utente e il numero del computer 
-
-//Stampo in console/pagina il vincitore
 
